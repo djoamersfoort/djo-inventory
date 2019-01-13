74eb187decb5cffec1401307b75540810ce4b15e
Ronald Moesbergen <mail@rmoesbergen.nl>
2019-01-13 12:36:36 +0100

    Add document table and field on item to store (PDF) docs of the item

--- inventory/models.py
+++ inventory/models.py
@@ -27,7 +27,7 @@ class Location(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=1024, default='')
-    photo = models.BinaryField(max_length=2000000, null=True, editable=True)
+    photo = models.BinaryField(max_length=4000000, null=True)
 
     def __str__(self):
         return "{0} ({1})".format(self.name, self.pk)
@@ -38,13 +38,19 @@ class Property(models.Model):
     name = models.CharField(max_length=255)
 
 
+class Document(models.Model):
+    id = models.AutoField(primary_key=True)
+    document = models.BinaryField(max_length=20000000)
+
+
 class Item(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=1024)
-    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, on_delete=models.PROTECT)
     url = models.URLField(verbose_name="Url of item specifications / docs")
     properties = models.ManyToManyField(Property)
+    document = models.ForeignKey(Document, on_delete=models.PROTECT, null=True)
 
     def __str__(self):
         return "{0} ({1})".format(self.name, self.pk)
